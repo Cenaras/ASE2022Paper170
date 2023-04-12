f77871c82d39a0cc8097f2570314a114e47736ef
cenaras <mathias_rud@hotmail.com>
2023-04-12 14:39:01 +0200

    update to test simple cycle stuff

--- Goat/benchmarks/all-benchmark.sh
+++ Goat/benchmarks/all-benchmark.sh
@@ -4,6 +4,9 @@ while read -r line
 do
   for entry in $line
   do
-    ./benchmarks/run.py --dir ${entry}_results --analysisStrategy ${entry}
+    if [[ ${entry:0:1} != "#" ]]; then
+      #echo $entry
+      ./benchmarks/run.py --dir ${entry}_results --analysisStrategy ${entry}
+    fi
   done
 done < "./benchmarks/strategies.txt"
